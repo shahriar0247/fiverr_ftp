3c0874112de21e8879c491c52fa375beaec813a8
Ahmed Shahriar <ahmedshahriar0247@gmail.com>
2021-05-21 16:56:57 +0600

    design done, working on logic

--- app/views/path/home.py
+++ app/views/path/home.py
@@ -1,8 +1,9 @@
+from flask.globals import request
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 
-@app.route("/path", methods=["POST","GET"])
+@app.route("/", methods=["POST","GET"])
 def home_view():
-    
-    return render_template('path/home.html', login=True)
+    all_folders = get_all_folders(request.form.args("path"))
+    return render_template('path/home.html', all_folders=all_folders)
