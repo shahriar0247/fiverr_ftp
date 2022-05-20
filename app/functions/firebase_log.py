25cea7aa08a2975d44af889ccf8caede73b011d1
Ahmed Shahriar <47810596+shahriar0247@users.noreply.github.com>
2022-05-20 20:21:32 +0600

    Update firebase_log.py

--- app/functions/firebase_log.py
+++ app/functions/firebase_log.py
@@ -4,14 +4,7 @@ from flask import session
 
 def firebase_initialize():
     firebase_config = {
-        "apiKey": "AIzaSyCXALNlXRdJrcsRHro_4mdysZbi-mkCP0U",
-        "authDomain": "indian-cloud-storage-fiverr.firebaseapp.com",
-        "projectId": "indian-cloud-storage-fiverr",
-        "storageBucket": "indian-cloud-storage-fiverr.appspot.com",
-        "messagingSenderId": "728035506508",
-        "appId": "1:728035506508:web:e5200e5276759bc45df7e9",
-        "measurementId": "G-J575XZM3PT",
-        "databaseURL": "",
+      
     }
 
     firebase = pyrebase.initialize_app(firebase_config)
